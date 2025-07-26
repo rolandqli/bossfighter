aab9c0bb11194cc2ef7f0fcaf1d3d2f3037885b8
Roland Li <liqi618@gmail.com>
2025-07-25 20:28:13 -0700

    reorganize

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,7 +15,12 @@ public class Player : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        Debug.Log(HPSlider);
         HPSlider.value = 1;
+        if (HPSlider == null)
+        {
+            Debug.Log("HPSlider is null");
+        }
     }
 
     // Update is called once per frame
@@ -24,20 +29,35 @@ public class Player : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 inputDir = new Vector3(x, 0, z);
+        if (x != 0 || z != 0)
+        {
+            Vector3 inputDir = new Vector3(x, 0, z);
 
-        Vector3 cameraZ = cameraTransform.forward;
-        Vector3 cameraX = cameraTransform.right;
-        cameraZ.y = 0f;
-        cameraX.y = 0f;
-        Vector3 relativeDir = cameraZ * z + cameraX * x;
-        Quaternion targetRotation = Quaternion.LookRotation(relativeDir);
-        
-        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
-        rb.MovePosition(rb.position + relativeDir * moveSpeed * Time.fixedDeltaTime);
-        float speed = rb.linearVelocity.magnitude;
+            Vector3 cameraZ = cameraTransform.forward;
+            Vector3 cameraX = cameraTransform.right;
+            cameraZ.y = 0f;
+            cameraX.y = 0f;
+            Vector3 relativeDir = cameraZ * z + cameraX * x;
+            Quaternion targetRotation = Quaternion.LookRotation(relativeDir);
+
+            //transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * 10f);
+            rb.MovePosition(rb.position + relativeDir * moveSpeed * Time.fixedDeltaTime);
+        }
+        //else
+        //{
+        //    anim.SetBool("Run", false);
+        //}
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            anim.SetTrigger("Kick");
+        }
+
+
         //anim.SetFloat("Speed", speed);s
-
+        void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Collided with: " + collision.gameObject.name);
+        }
 
 
     }
